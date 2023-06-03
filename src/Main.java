64ae5057435be196d1c48899145be9647b09d724
User <i.alhamamsha@outlook.com>
2023-06-03 23:04:26 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,37 @@
 // block size 280
 // record size 28
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 
-        System.out.println("Hello world!");
+public class Main {
+
+    public final static double BLOCK_SIZE = 280;
+    public final static double RECORD_SIZE = 28;
+    public final static double BLOCK_RECORDS = Math.ceil(BLOCK_SIZE/RECORD_SIZE);
+    public final static double size = 30_000; // number of records
+
+
+
+    public static void main(String[] args) {
+        List<Integer> records = new LinkedList<>();
+        List<Integer> indexes = new LinkedList<>(); // Primary index
+        BufferedReader in;
+
+        try{
+            in= new BufferedReader (new InputStreamReader(new FileInputStream("records.txt")));
+            while(in.ready()){
+                records.add(Integer.parseInt(in.readLine()));
+            }
+
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+
+
+        
+
     }
 }
